da6126c84ef38db2073f449df9f3e7bde1ff5403
mardi08 <mardi@purdue.edu>
2018-05-09 10:58:30 -0400

    added Geolocation API

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
 
+import requests
+
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/main.html')
+    response = requests.get('http://api.ipstack.com/128.210.106.81?access_key=32cf0d458d472abe5b6cc3f45d6c20e5')
+    geodata = response.json()
+    return render(request, 'accounts/main.html', {
+        'ip': geodata['ip'],
+        'country': geodata['country_name'],
+        'capital': geodata['location']['capital']
+    })
+    # return render(request, 'accounts/main.html')
